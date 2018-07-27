07650ad6093f3d4d8edc9373f6997ebab1d8af7f
Josh Hubers <jhubers@oaisd.org>
2018-07-27 13:30:03 -0400

    Added CreateService

---
+++ CreateService/ResourceCreateService.cs
@@ -0,0 +1,13 @@
+﻿using JsonApiDotNetCore.Services;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class ResourceCreateService : ICreateService<Resource, TId>
+    {
+      public virtual async Task<Resource> CreateAsync(Resource entity)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}

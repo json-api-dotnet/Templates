3290c49845bb1b751dd7f2dddbf8d23f73d63213
Josh Hubers <jhubers@oaisd.org>
2018-07-27 12:15:56 -0400

    Added ResourceQueryService

---
+++ ResourceQueryService/ModelResourceQueryService.cs
@@ -0,0 +1,30 @@
+﻿using JsonApiDotNetCore.Services;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace App
+{
+    public class ModelResourceGetRelationshipsService : IResourceQueryService<Model, TId>
+    {
+      public virtual async Task<IEnumerable<Model>> GetAsync()
+      {
+        throw new NotImplementedException();
+      }
+
+      public virtual async Task<Model> GetAsync(TId id)
+      {
+        throw new NotImplementedException();
+      }
+
+      public virtual async Task<object> GetRelationshipAsync(TId id, string relationshipName)
+      {
+        throw new NotImplementedException();
+      }
+
+      public virtual async Task<object> GetRelationshipsAsync(TId id, string relationshipName)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}

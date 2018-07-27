4fdb3167f2950a3bb889da1fdced9493fa1fa73b
Josh Hubers <jhubers@oaisd.org>
2018-07-27 13:32:47 -0400

    Added DeleteService

---
+++ DeleteService/ResourceDeleteService.cs
@@ -0,0 +1,13 @@
+﻿using JsonApiDotNetCore.Services;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class ResourceDeleteService : IDeleteService<Resource, TId>
+    {
+      public virtual async Task<bool> DeleteAsync(TId id)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}

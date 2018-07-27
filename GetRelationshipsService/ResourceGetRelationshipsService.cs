5adc7b92c0ec8a1a7bb022f624b5f31686392d64
Josh Hubers <jhubers@oaisd.org>
2018-07-27 11:57:18 -0400

    Added GetRelationshipsService

---
+++ GetRelationshipsService/ResourceGetRelationshipsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using JsonApiDotNetCore.Services;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class ResourceGetRelationshipsService : IGetRelationshipsService<Resource, TId>
+    {
+      public virtual async Task<object> GetRelationshipsAsync(TId id, string relationshipName)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}

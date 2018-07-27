6131a32d5b501b519419cbbd01c2c4ebc5c5b721
Josh Hubers <jhubers@oaisd.org>
2018-07-27 13:41:11 -0400

    Added UpdateRelationshipService

---
+++ UpdateRelationshipService/ResourceUpdateRelationshipService.cs
@@ -0,0 +1,14 @@
+﻿using JsonApiDotNetCore.Services;
+using System.Threading.Tasks;
+using JsonApiDotNetCore.Models;
+
+namespace App
+{
+    public class ResourceUpdateRelationshipService : IUpdateRelationshipService<Resource, TId>
+    {
+      public virtual async Task UpdateRelationshipsAsync(TId id, string relationshipName, List<DocumentData> relationships)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}

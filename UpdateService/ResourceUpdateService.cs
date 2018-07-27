66ce82309a9fd0b22e9e71560d739db2bb75e401
Josh Hubers <jhubers@oaisd.org>
2018-07-27 13:35:20 -0400

    Added UpdateService

---
+++ UpdateService/ResourceUpdateService.cs
@@ -0,0 +1,13 @@
+﻿using JsonApiDotNetCore.Services;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class ResourceUpdateService : IUpdateService<Resource, TId>
+    {
+      public virtual async Task<Resource> UpdateAsync(TId id, Resource entity)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}
